12b9a5cac8878b903fd7cff1074be102cb8c5387
Jiajun Chen <marsyeti@gmail.com>
2019-04-05 22:57:05 +0800

    add find method for eye

--- utils/eye.py
+++ utils/eye.py
@@ -1,7 +1,8 @@
-import subprocess
-import numpy as np
-import cv2
 import os
+import subprocess
+
+import cv2
+import numpy as np
 
 
 class Eye:
@@ -24,10 +25,37 @@ class Eye:
             self.temp_path, "temp.png"))
         subprocess.check_call(cmd)
 
-    def see(self):
+    def see(self, refresh=True):
         """read phone screen image, capture if not exist"""
         img_path = os.path.join(self.temp_path, "temp.png")
-        if not os.path.exists(img_path):
+        if not os.path.exists(img_path) or refresh:
             self._screen_shot()
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
         return img
+
+    def find(self, target_path, multi_target=False, threshold=0.95):
+        """
+        finds target center coord
+
+        returns None if nothing found
+        else returns [[x1, y1], [x2, y2], ...]
+        """
+        img = self.see()
+        target = cv2.imread(target_path, cv2.IMREAD_GRAYSCALE)
+        w_target, h_target = target.shape[::-1]  # shape of the target
+        res = cv2.matchTemplate(img, target, cv2.TM_CCOEFF_NORMED)
+        if res.max() < threshold:
+            return None
+
+        coords = []
+        max_val = 0
+        loc = np.where(res >= threshold)
+        for y, x in zip(*loc):
+            if multi_target:
+                coords.append([x + w_target // 2, y + h_target // 2])
+            else:
+                if res[y, x] > max_val:
+                    max_val = res[y, x]
+                    coords = [[x + w_target // 2, y + h_target // 2]]
+
+        return coords
