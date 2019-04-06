09c31aec2d5d9a2c31eb223e887e12ca8cece30d
Jiajun Chen <marsyeti@gmail.com>
2019-04-06 14:12:28 +0800

    add exam page for navigator

--- utils/navigator.py
+++ utils/navigator.py
@@ -11,7 +11,28 @@ class Navigator:
         self.eye = Eye(adb_path, temp_path)
         self.finger = Finger(adb_path, sleep_sec=sleep_sec)
 
-    def bottom_tab(self, n):
+    def score_page(self):
+        """click the score from main page"""
+        self._bottom_tab(2)
+        self._goto("score")
+
+    def exam_page(self):
+        """go to the exam page from main page"""
+        self._bottom_tab(4)
+        self._goto("exam_icon")
+
+    def scroll_up(self):
+        self.finger.swipe(500, 1000, 500, 500)
+
+    def scroll_down(self):
+        self.finger.swipe(500, 500, 500, 1000)
+
+    def _goto(self, img_name):
+        path = os.path.join("images", "{}.png".format(img_name))
+        coords = self.eye.find(path, multi_target=False)
+        self.finger.tap(*coords[0])
+
+    def _bottom_tab(self, n):
         """
         navigate to bottom n_th tab, the screen resolution is 1080x1920
 
@@ -28,10 +49,3 @@ class Navigator:
         x = [108 + 108 * 2 * i for i in range(5)]
         y = 1850
         self.finger.tap(x[n], y)
-
-    def check_score(self):
-        """click the score from main page"""
-        path = os.path.join("images", "score.png")
-        self.bottom_tab(2)
-        coords = self.eye.find(path, multi_target=False)
-        self.finger.tap(*coords[0])
